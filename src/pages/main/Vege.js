7e1e9d4598881cf67c1c4eb9c380e00d5651e31a
ChoiYunsik <ykuranus@gmail.com>
2024-12-26 13:49:49 +0900

    main페이지 사진 변경

--- src/pages/main/Vege.js
+++ src/pages/main/Vege.js
@@ -2,6 +2,10 @@ import React from 'react';
 import S from './style';
 import { useNavigate } from 'react-router-dom';
 import Rectangle from './images/Rectangle.png'
+import Product01 from './images/product01.svg'
+import Product02 from './images/product02.svg'
+import Product03 from './images/product03.svg'
+import Product04 from './images/product04.svg'
 import ItemContainer from './ItemContainer';
 
 const Vege = () => {
@@ -13,28 +17,28 @@ const Vege = () => {
                 title="아보카도"
                 description="유기농 아보카도 3+1 개입"
                 price="8,700원"
-                image={Rectangle}
+                image={Product01}
                 onNavigate={() => navigate("/details")}
             />
             <ItemContainer
                 title="방울토마토"
                 description="유기농 방울토마토 500g"
                 price="7,990원"
-                image={Rectangle}
+                image={Product02}
                 onNavigate={() => navigate("/details")}
             />
             <ItemContainer
-                title="옥수수"
-                description="유기농 옥수수 2개입"
+                title="마늘"
+                description="유기농 마늘 4개입"
                 price="9,990원"
-                image={Rectangle}
+                image={Product03}
                 onNavigate={() => navigate("/details")}
             />
             <ItemContainer
-                title="딸기"
-                description="유기농 딸기 500g"
+                title="브로콜리"
+                description="유기농 브로콜리 500g"
                 price="12,990원"
-                image={Rectangle}
+                image={Product04}
                 onNavigate={() => navigate("/details")}
             />
         </S.TagItemWrapper>
