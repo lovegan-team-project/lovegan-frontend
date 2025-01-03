76e509a3ebf043afea44b4951e8de467699f4f4a
ChoiYunsik <ykuranus@gmail.com>
2025-01-03 20:33:24 +0900

    제품상세페이지 수정

--- src/pages/product/ProductDetails.jsx
+++ src/pages/product/ProductDetails.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from './style';
 import Narrow from './images/icon.png'
 import MainPic from './images/productdetailMain.png'
 import MainSubPic from './images/Rectangle 340.png'
 import SubPic from './images/Rectangle 341.png'
 import Star from './images/star.png'
+import X from './images/X.png'
+import Like from './images/icon (6).png'
+import LikeClick from '../main/images/LikeClick.svg'
+import ProductDetailPic from './images/ProductDetailPic.png'
 const ProductDetails = () => {
-    window.scrollTo(0,0);
+    
+    // window.scrollTo(0,0);
+    const [like, setLike] = useState(false);
+    
+    const toggleLike = () => setLike(!like);
     return (
         
         <>
@@ -65,18 +73,70 @@ const ProductDetails = () => {
                                 </S.OptionResultBox>
                         </S.OrderDetailContainer>
                         <S.OrderPriceWrapper>
-                            <div >주문금액</div>
+                            <div>주문금액</div>
                             <S.OrderTotalPrice>65,800원</S.OrderTotalPrice>
+                            
                         </S.OrderPriceWrapper>
+                        <S.ProductDetailCartButton>장바구니</S.ProductDetailCartButton>
+                        <S.ProductDetailBuyButton>바로구매</S.ProductDetailBuyButton>
                     </S.DetailContainer>
                 </S.DetailMainWrapper>
 
             </S.ProductDetailContainer>
                 
             <S.ProductDetailBar>
-
+            <S.BarProductInfo>상품정보</S.BarProductInfo>
+            <S.BarReivew>리뷰<span>152</span></S.BarReivew>
+            <S.BarQnA>문의<span>23</span></S.BarQnA>
+            <S.BarDeliveryAndRefund>배송/환불</S.BarDeliveryAndRefund>
+            <S.BarSimilarProduct>비슷한 상품</S.BarSimilarProduct>
             </S.ProductDetailBar>
             <S.ProductOptionCheckContainer>
+                <S.ProductPicContainer>
+                    <S.ProductPicWrapper><img src={ProductDetailPic} alt="pic1" /></S.ProductPicWrapper>
+                    <S.ProductPicWrapper><img src={ProductDetailPic} alt="pic2" /></S.ProductPicWrapper>
+                    <S.ProductPicWrapper><img src={ProductDetailPic} alt="pic3" /></S.ProductPicWrapper>
+                </S.ProductPicContainer>
+                
+                <S.PayOptionBox>
+                <S.ProductOptionDetail2>
+                    <div>옵션 선택</div> 
+                    <select>
+                        <option>상품 A</option>
+                        <option>상품 B</option>
+                        <option>상품 C</option>
+                    </select>
+                </S.ProductOptionDetail2>
+                <S.MiniOption>
+                    <div><S.MiniProductName>상품 A</S.MiniProductName><S.XButton><img src={X}alt="x" /></S.XButton></div>
+                    <S.MiniButtonPriceWrapper>
+                        <S.AmountWrapper><button>-</button><span>1</span><button>+</button></S.AmountWrapper>
+                        <S.MiniPrice>65,800원</S.MiniPrice>
+                    </S.MiniButtonPriceWrapper>
+                </S.MiniOption>
+                <S.TotalPayWrapper>
+                    <S.OrderPriceWrapper2>
+                        <div>주문금액</div>
+                        <S.OrderTotalPrice>65,800원</S.OrderTotalPrice>
+
+                    </S.OrderPriceWrapper2>
+                    <S.MiniButtonWrapper>
+                        <S.LikeDiv><img src={like ? LikeClick : Like} onClick={toggleLike} className='like'></img></S.LikeDiv>                        
+                        <S.ProductDetailCartButton2>장바구니</S.ProductDetailCartButton2>
+                        <S.ProductDetailBuyButton2>바로구매</S.ProductDetailBuyButton2>
+
+                    </S.MiniButtonWrapper>
+
+
+
+
+                </S.TotalPayWrapper>
+                </S.PayOptionBox>
+                
+                
+                
+
+                
 
             </S.ProductOptionCheckContainer>
         </>
