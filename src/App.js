6fb557c4b1305791233ff87975ffbad7a48c9aaa
yoohyun-1203 <yoohyun9448@gmail.com>
2025-01-10 04:42:06 +0900

    로그인 페이지 수정

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import router from './routes/router';
 import GlobalStyle from './global/global';
 import { ThemeProvider } from 'styled-components';
 import theme from './global/theme';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(()=>{
+    if(localStorage.getItem("token")){
+      const isAuthenticate = async () => {
+        const response = await fetch("http://localhost:8000/auth/jwt", {
+          method : "POST",
+          headers : {
+            'Authorization' : `Bearer ${localStorage.getItem("token")}`
+          }
+        })
+
+        if(!response.ok) return;
+
+        const getAuthenticate = await response.json();
+        return getAuthenticate;
+      }
+
+      isAuthenticate()
+      .then((res) => {
+        console.log(res)
+      })
+    }
+  }, [])
   
   return (
     <>
