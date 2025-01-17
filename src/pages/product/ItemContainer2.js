eca6f699d92aa1515dc7c71a1b2a635ef96f74c3
ChoiYunsik <ykuranus@gmail.com>
2025-01-17 12:20:55 +0900

    product 데이터 가져와서 뿌리기 성공

--- src/pages/product/ItemContainer2.js
+++ src/pages/product/ItemContainer2.js
@@ -5,6 +5,7 @@ import LikeClick from '../main/images/LikeClick.svg'
 import { useNavigate } from 'react-router-dom';
 import Image from '../main/images/Rectangle.png';
 
+
 const ItemContainer2 = () => {
     const [like, setLike] = useState(false);
 
