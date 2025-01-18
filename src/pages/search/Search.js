cddb88119058b3cfe51249f95d985d31f7b6db90
hey-yoon <yooniyunisss@gmail.com>
2025-01-18 15:56:42 +0900

    search부분 데이터 가져오기

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import S from './style';
 import { Link } from 'react-router-dom';
 import FoodComponent from './FoodComponent';
@@ -19,66 +19,149 @@ const Search = () => {
     // query(키) 의 값을 들고옴
     const searchQuery = queryParams.get('query');
 
+    // 가져온 데이터 기본값
+    const [foodData,setFoodData] = useState([]);
+    // const [otherData,setOtherData] = useState([]);
+    const [communityData,setCommunityData] = useState([]);
+    const [restaurantData,setRestaurantData] = useState([]);
+
+    // useEffect(()=> {
+    //     setSearchData(searchQuery)
+    // },[]);
+
+    // console.log(searchData);
+
+    // food 데이터 가져오기 로직
+    useEffect(()=>{
+        const getFood = async() => {
+            try{
+                const response = await fetch("http://localhost:8000/product/get")
+                // console.log(response);
+                const data = await response.json();
+                
+                const filteredData = data.filter((item) => item.title.includes(searchQuery));
+
+                // console.log(filteredData);
+
+                setFoodData(filteredData);
+            }
+            catch(error){
+                console.error("API호출 오류",error);
+            }
+        }
+        getFood()
+    },[searchQuery]); 
+
+    // other데이터 가져오기 로직
+    // useEffect(()=>{
+    //     const getOther = () => {
+    //         try{
+
+    //         }
+    //         catch(error){
+    //             console.error("error",error)
+    //         }
+    //     }
+    //     getOther();
+
+    // },[searchQuery]);
+
+    // community 데이터 가져오기 로직
+    useEffect(()=>{
+        const getCommunity = async() => {
+            try{
+                const response = await fetch("http://localhost:8000/community/getPost");
+                // console.log(response);
+                const data = await response.json();
+                // console.log(data);
+                const filteredData = data.filter((item) => item.title.includes(searchQuery));
+                // console.log(filteredData);
+                setCommunityData(filteredData);
+                }
+            catch(error){
+                console.error("error",error);
+            }
+            
+        }
+        getCommunity()
+    },[searchQuery]);
+
+    // restaurant 데이터 가져오기 로직
+    useEffect(()=>{
+        const getRestaurant = async() => {
+            try{
+                const response = await fetch("http://localhost:8000/restaurant/get");
+                console.log(response);
+                const data = await response.json();
+                console.log(data);
+                const filteredData = data.filter((item) => item.name.includes(searchQuery));
+                setRestaurantData(filteredData);
+            }
+            catch(error){
+                console.error("error",error);
+            }
+        }
+        getRestaurant();
+    },[searchQuery]);
+   
+
+
     return (
-        <S.Wrapper>
-            <h4>'{searchQuery}'에 대한 검색 결과</h4>
-            <hr className='line' />
-            {/* food 검색결과 */}
-            <S.Food>
-                <S.Title>
-                    <h5>FOOD <span>1,327</span></h5>
-                    <Link to="/">더보기</Link>
-                </S.Title>
-                <div style={{display:"flex", gap: "19px"}}>
-                    <FoodComponent />
-                    <FoodComponent />
-                    <FoodComponent />
-                    <FoodComponent />
+        (foodData.length && /* otherData.length &&  */communityData.length && restaurantData.length === 0 ?
+        (<S.noWrapper className='noSearch'>
+            <h4>'{searchQuery}'에 대한 검색 결과가 없습니다.</h4>
+        </S.noWrapper>)
+        : ( 
+            <S.Wrapper>
+                <h4>'{searchQuery}'에 대한 검색 결과</h4>
+                <hr className='line' />
+                {/* food 검색결과 */}
+                <S.Food>
+                    <S.Title>
+                        <h5>FOOD <span>{foodData.length}</span></h5>
+                        {foodData.length > 3 && (<Link to="/product">더보기</Link>)}
+                    </S.Title>
+                    <div style={{display:"flex", gap: "19px"}}>
+                        <FoodComponent foodData={foodData} />
+                    </div>
+                </S.Food>
+                <hr className='line' />
+    
+                {/* other 검색결과 */}
+                <div className="other"></div>
+                    <S.Title>
+                        <h5>OTHER <span>{/* {otherData.length} */}</span></h5>
+                        <Link to="/product">더보기</Link>
+                    </S.Title>
+                    <div style={{display:"flex", gap: "19px"}}>
+                        <OtherComponent /* otherData={otherData}  *//>
+                    </div>
+                <hr className='line' />
+    
+                {/* community 검색결과 */}
+                <div className="community">
+                    <S.Title>
+                        <h5>COMMUNITY <span>{communityData.length}</span></h5>
+                       {communityData.length > 2 && (<Link to="/community">더보기</Link>)}
+                    </S.Title>
+                    <div style={{display: "flex",gap: "19px"}}>
+                        <CommunityComponent communityData={communityData} />
+                    </div>
                 </div>
-            </S.Food>
-            <hr className='line' />
-
-            {/* other 검색결과 */}
-            <div className="other"></div>
-                <S.Title>
-                    <h5>OTHER <span>1,327</span></h5>
-                    <Link to="/">더보기</Link>
-                </S.Title>
-                <div style={{display:"flex", gap: "19px"}}>
-                    <OtherComponent />
-                    <OtherComponent />
-                    <OtherComponent />
-                    <OtherComponent />
+                <hr className='line' />
+    
+                {/* restaurant 검색결과 */}
+                <div className="restaurant">
+                    <S.Title>
+                        <h5>RESTAURANT <span>{restaurantData.length}</span></h5>
+                         {restaurantData.length > 2 && (<Link to="/restaurant">더보기</Link>)}
+                    </S.Title>
+                    <div style={{display:"flex",gap: "19px"}}>
+                        <RestaurantComponent restaurantData={restaurantData} />
+                    </div>
                 </div>
-            <hr className='line' />
-
-            {/* community 검색결과 */}
-            <div className="community">
-                <S.Title>
-                    <h5>COMMUNITY <span>1,327</span></h5>
-                    <Link to="/">더보기</Link>
-                </S.Title>
-                <div style={{display: "flex",gap: "19px"}}>
-                    <CommunityComponent />
-                    <CommunityComponent />
-                    <CommunityComponent />
-                </div>
-            </div>
-            <hr className='line' />
-
-            {/* restaurant 검색결과 */}
-            <div className="restaurant">
-                <S.Title>
-                    <h5>RESTAURANT <span>1,327</span></h5>
-                    <Link to="/">더보기</Link>
-                </S.Title>
-                <div style={{display:"flex",gap: "19px"}}>
-                    <RestaurantComponent />
-                    <RestaurantComponent />
-                    <RestaurantComponent />
-                </div>
-            </div>
-        </S.Wrapper>
+            </S.Wrapper>)
+    )
     );
 };
 
