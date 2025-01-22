845b7be49d22c9e932976b74f0c0d09506501451
kimhanbii <hanbe0109@gmail.com>
2025-01-22 15:03:10 +0900

    댓글 수 증가 성공

--- src/pages/community/CommentForm.jsx
+++ src/pages/community/CommentForm.jsx
@@ -3,7 +3,7 @@ import S from './style';
 import UserImg2 from './image/UserImg2.png';
 
 // parentId=null, onReply
-const CommentForm = ( {addList, parentId, addReply} ) => {
+const CommentForm = ( {addList, author, addReply} ) => {
     const [value, setValue] = useState('');
     
     // const now = new Date();   
@@ -14,24 +14,26 @@ const CommentForm = ( {addList, parentId, addReply} ) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
+
         if(value.trim()) {
             const newComment = {
-                post: "유저",
-                author: '유저 닉네임',
+                // author: author,
                 content: value, // 입력한 댓글 내용
                 // 대댓글 저장 배열
-                replies: [],
-                createAt: new Date().toISOString(),
+                // replies: [],
+                // createAt: new Date().toISOString(),
             };
 
-            if(parentId) {
-                addReply(parentId, newComment);
-            } else {
-                addList(newComment);
-            }
-
+            // if(parentId) {
+            //     addReply(parentId, newComment);
+            // } else {
+            //     addList(newComment);
+            // }
+            
+            addList(newComment)
             setValue('');
-            console.log('Parent ID:', parentId);
+
+            console.log('Parent ID:', author);
             console.log('addReply Function:', addReply);
             console.log('New Comment:', newComment);
         }
@@ -43,14 +45,15 @@ const CommentForm = ( {addList, parentId, addReply} ) => {
             <form onSubmit={handleSubmit}>
                 <span className='box'>
                     <input 
-                        type="text"
                         className='int'
-                        placeholder={
-                            parentId 
-                            ? '대댓글 입력' 
-                            : '칭찬과 격려의 댓글은 작성자에게 큰 힘이 됩니다 :)'}
-                        onChange={handleChange}
+                        type="text"
                         value={value}
+                        onChange={handleChange}
+                        placeholder={
+                            // parentId 
+                            // ? '대댓글 입력' 
+                            // : 
+                        '칭찬과 격려의 댓글은 작성자에게 큰 힘이 됩니다 :)'}
                     />
                 </span>
                 <button type="submit" className='submitBt' disabled={!value.trim()}>
